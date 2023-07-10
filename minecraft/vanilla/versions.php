30db1ac539543259fe5d567222bcdbfd17227868
Syhix <cococraft85@gmail.com>
2023-07-10 17:52:51 +0200

    add minecraft vanilla & forge manifest

---
+++ minecraft/vanilla/versions.php
@@ -0,0 +1,28 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+// Connexion à la base de données MySQL
+$conn = new mysqli('91.121.230.45', 'mcvanilla-canary', 'syhixmcvanillacanary', 'mcvanilla-canary');
+
+// Vérification de la connexion
+if ($conn->connect_error) {
+    die('Erreur de connexion (' . $conn->connect_errno . ') ' . $conn->connect_error);
+}
+
+// Exécution de la requête SQL pour extraire les données de la base de données
+$resultat = $conn->query('SELECT * FROM `mcversions`');
+
+// Création d'un tableau associatif pour stocker les données
+$tableau_data = array();
+while ($ligne = $resultat->fetch_assoc()) {
+    $tableau_data[] = $ligne;
+}
+
+// Conversion des données en format JSON
+$json_data = json_encode($tableau_data, JSON_PRETTY_PRINT);
+
+// Affichage des données en format JSON
+$json_data = str_replace("\\/", "/", $json_data);
+echo $json_data;
+
+?>
