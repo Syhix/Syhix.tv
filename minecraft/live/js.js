2f556a756d40944c0fcb52d7880ae24f4886f11e
Syhix <cococraft85@gmail.com>
2023-09-16 19:41:43 +0200

    add Minecraft Live page countdown

---
+++ minecraft/live/js.js
@@ -0,0 +1,23 @@
+const text = document.getElementById('time')
+
+
+function timeCouldown() {
+    const now = new Date().getTime();
+    const countdownDate = new Date('Oct 15, 2023 : 19:00').getTime();
+    const distanceBase = countdownDate - now;
+
+    if (distanceBase < 0) {
+        clearInterval(countDownInterval);
+    }
+
+    const days = Math.floor(distanceBase / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distanceBase % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distanceBase % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distanceBase % (1000 * 60)) / 1000);
+
+    text.innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
+}
+
+const countdownInterval = setInterval(() => {
+    timeCouldown()
+}, 1000)
